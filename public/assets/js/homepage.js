25ffe36c41862dd21ff9d622aed048921d0da566
Nicole Varela <nicolevarela@nicoles-mbp-2.mynetworksettings.com>
2024-12-17 16:47:09 -0500

    commit 3

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,24 +1,25 @@
-// Adds JS greeting to homepage based on the time
 console.log('calling homepage');
 
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18) {
-    greeting = 'Good evening, welcome to my online resume!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon, welcome to my online resume!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning, welcome to my online resume!';
-} else {
-    greeting = 'Welcome!';
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+    //Greeting based on the time of day
+    if (hourNow > 18) {
+        greeting = 'Good evening, welcome to my online resume!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon, welcome to my online resume!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning, welcome to my online resume!';
+    } else {
+        greeting = 'Welcome!';
+    }
+
+
     var greetingContainer = document.getElementById('greeting');
     greetingContainer.textContent = greeting;
-
-    greetingContainer.style.fontSize = "30";
+    greetingContainer.style.fontSize = "33px";
     greetingContainer.style.fontWeight = "bold";
+    greetingContainer.style.color = "#B3A098";
 });
